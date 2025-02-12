334f81d39e570b79fbd5c08540fe9afe618498e5
Lucas Caridi <w5437253@macmini05.gbccloud.com>
2025-02-12 00:05:09 -0500

    Start button

--- Lab1_Lucas_Caridi/ContentView.swift
+++ Lab1_Lucas_Caridi/ContentView.swift
@@ -8,16 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number: Int = Int.random(in: 1...100)
+    @State private var isQuizActive = false
+    @State private var result: String = ""
+    @State private var correct = 0
+    @State private var wrong = 0
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack{
+                Button(action: {toggleQuiz()}, label: {
+                    Text(isQuizActive ? "Stop Prime Quiz" : "Start Prime Quiz")
+                })
+                .font(.largeTitle)
+                .padding()
+                .background(isQuizActive ? Color.red : Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(5)
+                .padding()
+            }
         }
-        .padding()
+    }
+    
+    func toggleQuiz() {
+        isQuizActive.toggle()
+        if isQuizActive {
+            number = Int.random(in: 1...100)
+            result = ""
+            correct = 0
+            wrong = 0
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
