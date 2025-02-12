073d22d6eb9eb188269855590ddd0fee84d2c9c6
Lucas Caridi <w5437253@macmini05.gbccloud.com>
2025-02-11 21:59:18 -0500

    project start

---
+++ Lab1_Lucas_Caridi/Lab1_Lucas_CaridiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Lucas_CaridiApp.swift
+//  Lab1_Lucas_Caridi
+//
+//  Created by Lucas Caridi on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Lucas_CaridiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
